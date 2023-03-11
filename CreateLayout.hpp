bad2c49a5077e85852e2de3d6285f41fb1c7b576
Victor Zenin <victor.zenin.o@gmail.com>
2023-03-11 16:28:35 +0300

    removed unnecessaty header

--- CreateLayout.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <vector>
-
-namespace NodesGenerator {
-std::vector<double> chebyshev(size_t N, double leftBound, double rightBound);
-std::vector<double> uniform(size_t N, double leftBound, double rightBound);
-std::vector<double> random(size_t N, double leftBound, double rightBound);
-}
