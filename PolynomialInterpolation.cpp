f33d00f6caf05ac27e08f990ef5ecf8daa050278
Victor Zenin <victor.zenin.o@gmail.com>
2023-03-21 09:50:24 +0300

    started valle. does not to compile now

--- PolynomialInterpolation.cpp
+++ PolynomialInterpolation.cpp
@@ -2,8 +2,10 @@
 #include "GaussianElimination.hpp"
 #include "Utils.hpp"
 
+#include <algorithm>
 #include <cmath>
 #include <iostream>
+#include <numeric>
 
 std::vector<double> getModifiedVandermondeMatrix(
     const std::vector<double>& x, size_t N);
@@ -36,22 +38,76 @@ std::vector<double> getCanonicalCoefficients(
 	return gaussianEliminationPickMaxColumn(A, y);
 }
 
-int main()
+std::vector<size_t> getBasisIndices(unsigned n_coeffs_with_h)
+{
+	auto basisIds = std::vector<size_t>(n_coeffs_with_h);
+
+	std::iota(basisIds.begin(), basisIds.end(), 1);
+
+	return basisIds;
+}
+
+std::pair<std::vector<double>, std::vector<double>> getBasis(
+    const std::vector<double>& x, const std::vector<double>& y,
+    const std::vector<size_t>& basisIds)
+{
+	auto basisX = std::vector<double>(basisIds.size());
+	auto basisY = std::vector<double>(basisIds.size());
+
+	for (auto idx : basisIds) {
+		basisX.push_back(x[idx]);
+		basisY.push_back(y[idx]);
+	}
+
+	return std::pair<std::vector<double>, std::vector<double>>(basisX, basisY);
+}
+
+std::vector<double> getValleePoussinCanonicalCoefficitnts(
+    const std::vector<double>& x, const std::vector<double>& y,
+    const size_t nCoefficientsWithH)
+{
+	auto basisIds = getBasisIndices(nCoefficientsWithH);
+
+	do {
+		auto& [basisX, basisY] = getBasis(x, y, basisIds);
+
+		try {
+			auto getCanonicalCoefficients(basisX, basisY);
+		} catch (std::runtime_error& e) {
+			break;
+		}
+
+	} while (true);
+}
+
+int main(int argc, char* argv[])
 {
 	std::cin.exceptions(std::ios::failbit | std::ios::badbit);
 	size_t N;
+	size_t polynomialDegree;
 
+	if (argc >= 2)
+		polynomialDegree = std::stoull(argv[1]);
+
+	// the number of points
+	// should be greatest then or equeal to polynomail degree
+	// for vallee poussin method
 	std::cin >> N;
+
+	if (polynomialDegree >= N - 1)
+		return -1;
 
 	auto x = std::vector<double>(N);
 	auto y = std::vector<double>(N);
 
-	for (size_t i = 0; i < N; ++i) {
-		std::cin >> x[i];
-		std::cin >> y[i];
-	}
+	for (size_t i = 0; i < N; ++i)
+		std::cin >> x[i] >> y[i];
 
-	auto canonicalCoefs = getCanonicalCoefficients(x, y);
+	size_t nCoefficientsWithH = polynomialDegree + 2;
+
+	auto canonicalCoefs = (nCoefficientsWithH == N)
+	    ? getCanonicalCoefficients(x, y)
+	    : getValleePoussinCanonicalCoefficitnts(x, y, nCoefficientsWithH);
 
 	printResult(x, canonicalCoefs);
 
